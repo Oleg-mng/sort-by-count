1b04c4b3bf51ba221e34ad8685c206419dc6d791
oleg <legik7127@gmail.com>
2022-08-12 21:32:00 +0300

    done sortby count

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿int[] array = { 0, 2, 3, 2, 1, 5, 9, 1, 1, 3, 3, 5, 5, 7, 9, 7};
+CountingSort (array);
+System.Console.WriteLine(string.Join(", ", array));
+void CountingSort(int[] inputArray)
+{
+    int[] counters = new int[10];
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        counters[inputArray[i]]++;
+        // int ourNumber = inputArray[i];
+        // counters[ourNumber]++;
+    }
+    int index = 0;
+    for (int i = 0; i < counters.Length; i++)
+    {
+        for (int j = 0; j < counters[i]; j++)
+        {
+            inputArray[index] = i;
+            index++;
+        }
+    }
+}
